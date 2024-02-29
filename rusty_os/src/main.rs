aefc51761bfa05878f77ef93aa855841b6e903bc
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-02-29 19:32:10 +0700

    progress test
    i can't figure it out yet how to run the test, so I decided to copy the code and adjusting it, and move on

--- rusty_os/src/main.rs
+++ rusty_os/src/main.rs
@@ -1,73 +1,14 @@
 #![no_std] // disable linking into the standard library
 #![no_main] // tell the rust compiler to not use the normal entry point
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(rusty_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
+use rusty_os::println;
+use rusty_os::QemuExitCode;
+use rusty_os::serial_println;
 use core::panic::PanicInfo;
 
-#[cfg(not(test))]
-#[panic_handler]
-pub fn panic(_info: &PanicInfo) -> ! {
-    println!("{}", _info);
-    loop {}
-}
-
-#[cfg(test)]
-#[panic_handler]
-pub fn test_panic(_info: &PanicInfo) -> ! {
-    println!("{}", _info);
-    loop {}
-}
-
-
-// #[cfg(test)]
-// #[panic_handler]
-// fn panic(info: &PanicInfo) -> ! {
-//     crate::test_panic_handler(info)
-// }
-
-// TEST RUNNER
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Initiating Test Runner");
-    println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
-
-    exit_qemu(QemuExitCode::Success)
-}
-
-// TEST CASESS
-#[test_case]
-pub fn trivial_assertion() {
-    print!("trivial assertion... ");
-    assert_eq!(1, 1);
-    println!("[ok]");
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-/*
-By using the #[no_mangle] attribute,
-we disable name mangling to ensure that the Rust compiler
-really outputs a function with the name _start.
-Without the attribute, the compiler would generate some cryptic _ZN3blog_os4_start7hb173fedf945531caE
-*/
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
@@ -79,4 +20,25 @@ pub extern "C" fn _start() -> ! {
 
     println!("bebek");
     loop {}
+}
+
+
+// TEST RUNNER
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rusty_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
 }
--- rusty_os/target/debug/deps/rusty_os-e201fbb558de7439.d
+++
@@ -1,5 +0,0 @@
-/Users/asthiseta/RustroverProjects/LearningRust/rusty_os/target/debug/deps/rusty_os-e201fbb558de7439: src/main.rs
-
-/Users/asthiseta/RustroverProjects/LearningRust/rusty_os/target/debug/deps/rusty_os-e201fbb558de7439.d: src/main.rs
-
-src/main.rs:
